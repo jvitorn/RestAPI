660d5a8869e228d58b99ea04bfbbf4fc0086601a
jvitorn <joaoeuno2011@gmail.com>
2020-04-13 09:50:59 -0300

    02 - Isolando Rotas e adicionando BodyParser

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,15 +1,20 @@
 // require
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 module.exports = ()=>{
     // criando um app em express
     const app = express();
+    //use midwares
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.json());
     //consign esta incluindo tudo que esta na pasta 'controllers' para dentro do app
     consign()
         .include('controllers')
         .into(app)
-        
+     
+
     return app;
     
 }
--- controllers/atendimentoController.js
+++ controllers/atendimentoController.js
@@ -1,6 +1,11 @@
 
 module.exports =  (app) => {
-    app.get('/atendimentos',(req,res)=>{
-        res.send('Voce esta na rota de atendimentos pelo controller');
-    });
+    app.route('/atendimentos')
+        .get((req,res)=>{
+            res.send('Voce esta na rota de atendimentos pelo controller GET');
+        })
+        .post((req,res)=>{
+            console.log(req.body);
+            res.send('Voce esta na rota de atendimentos pelo controller POST');
+        });
 }
